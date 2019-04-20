13ed135ebc98c7d85e8a85ee34ceecac78c00a37
Prakcoder <Prakashverma042@gmail.com>
2019-04-20 16:24:40 +0530

    Added board module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        MainComponent
+        MainComponent,
+        BoardComponent
     ],
     imports: [
         BrowserModule,
