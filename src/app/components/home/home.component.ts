9712c816ef2a10b314ac1a96835f3969d5ec718b
Prakcoder <Prakashverma042@gmail.com>
2019-04-20 17:18:06 +0530

    Code implemented in component to load game and   save

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { SAVED_GAME_ID } from 'src/app/functions/utils';
+import { GameService } from 'src/app/services/game.service';
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    @Output() newGame = new EventEmitter();
+    @Output() resetGame = new EventEmitter();
+    @Output() loadGame = new EventEmitter();
 
-  ngOnInit() {
-  }
+    public showNoGameSaved: boolean;
+
+    constructor(private gameService: GameService) { }
+
+    ngOnInit() {
+    }
+
+    public startNewGame() {
+        this.newGame.emit();
+    }
+
+    private closeMessage() {
+        this.showNoGameSaved = false;
+    }
+
+    public loadSavedGame() {
+        if (!this.gameService.isSavedGame()) {
+            this.showNoGameSaved = true;
+            return;
+        }
+        this.loadGame.emit(SAVED_GAME_ID);
+    }
+
+    public resetGamestate() {
+        this.resetGame.emit();
+    }
 
 }
