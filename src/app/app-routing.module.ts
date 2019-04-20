f0da73ffd29aeddf8f1f519ad043bec6e55acf2c
Prakcoder <Prakashverma042@gmail.com>
2019-04-20 16:21:00 +0530

    Added main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './components/main/main.component';
+
+const routes: Routes = [
+    { path: '', components: MainComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
